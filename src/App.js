d563ce938a88a846415ce8f18027c479ce3b997e
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-11-06 20:23:12 +0700

    Disable long polling

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ import "./App.css";
 
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
-const socket = io(SOCKET_URL);
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+});
 
 const columns = [
   {
