9b179dfb288d65cea9cd0cd1ea79454c84d27793
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-11-06 20:42:24 +0700

    Add polling too

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import "./App.css";
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["websocket","polling"],
 });
 
 const columns = [
